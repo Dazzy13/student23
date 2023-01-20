622a70736f7da597ea1a243435cf683e8fe536d7
Dazzling Verdi <verdiorn@sheridancollege.ca>
2023-01-20 13:42:41 -0500

    First Commit

---
+++ src/student/ReverseWord.java
@@ -0,0 +1,29 @@
+
+package student;
+import java.util.Scanner;
+public class ReverseWord {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        
+        String userInput = scan.nextLine();
+        
+        char[] myLetters = new char[userInput.length()];
+        
+        for(int i = 0; i < myLetters.length; i++)
+        {
+            myLetters[i] = userInput.charAt(i);
+            
+            System.out.println(myLetters[i]);
+            
+        }
+        
+        for(int i = myLetters.length - 1; i >= 0; i--)
+        {
+            System.out.println(myLetters[i]);
+        }
+
+               
+    }
+    
+}
---
+++ src/student/StudentMain.java
@@ -0,0 +1,19 @@
+
+package student;
+
+public class StudentMain {
+    public static void main(String[] args){
+          Student[] studentList = new Student[3];
+       studentList[0] = new Student("90", "Dazz");
+       studentList[1] = new Student("91", "Jim");
+       studentList[2] = new Student("92", "Lainey");
+       
+       for(int i =0; i < studentList.length; i++) 
+       {
+           System.out.println(studentList[i].getStudentName());
+       }
+    }
+     
+
+    
+}
